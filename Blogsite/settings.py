7c1b1eeef18dff8ec0214fdffe1d65d97b0f5bef
Arrjoon <nepaliarjun049@gmail.com>
2024-09-01 23:45:10 +0545

    new file added

--- Blogsite/settings.py
+++ Blogsite/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-
-STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,9 @@ set -xe
 # Install dependencies
 python3 -m pip install -r requirements.txt
 
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
 # Run Django collectstatic
 python3 manage.py collectstatic --noinput
+
