da286350f173dc59f5cddf9907d83aeec87d4597
Arrjoon <nepaliarjun049@gmail.com>
2024-09-01 23:24:31 +0545

    some changes

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Print commands and exit on error
+set -xe
+
+# Install dependencies
 python3 -m pip install -r requirements.txt
+
+# Run Django collectstatic
 python3 manage.py collectstatic --noinput
